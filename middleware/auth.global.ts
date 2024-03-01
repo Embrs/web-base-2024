f90100777515ae4bb34035dd27a74bd42fb2a1b9
鄒豐懋 <zoufengmao@zoufengmaodeMacBook-Pro.local>
2024-03-01 13:47:02 +0800

    add middleware

---
+++ middleware/auth.global.ts
@@ -0,0 +1,8 @@
+export default defineNuxtRouteMiddleware((to, from) => {
+  // console.log(to, from);
+  // console.log("middleware demo", to, from);
+  // if (to.params.id === '1') {
+  //   return abortNavigation() // 終止導航
+  // }
+  // return navigateTo('/') // 導航
+});
---
+++ pages/aaa.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+// AAA 請填寫功能描述👈
+</script>
+
+<template lang="pug">
+#AAA
+  p AAA
+</template>
+
+<style lang="scss" scoped>
+// 佈局 ----
+#AAA {
+  // TODO
+}
+
+// 組件 ----
+</style>
