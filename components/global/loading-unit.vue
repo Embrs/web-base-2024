63473bdbab0a6ad1ba95072eadb8d80cf4eef1c4
鄒豐懋 <zoufengmao@zoufengmaodeMacBook-Pro.local>
2024-03-06 10:06:29 +0800

    add loading

---
+++ components/global/loading-unit.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+#LoadingUnit(v-loading="true")
+</template>
+
+<style lang="scss" scoped>
+// 佈局 ----
+#LoadingUnit {
+  @include wh;
+  @include center;
+  font-size: 40px;
+}
+// 組件 ----
+</style>
---
+++ components/global/loading.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+Transition(name="com" mode="out-in")
+  Suspense
+    slot
+    template(#fallback)
+      #Loading(v-loading="true")
+</template>
+
+<style lang="scss" scoped>
+// 佈局 ----
+#Loading {
+  @include wh;
+  @include center;
+  font-size: 40px;
+}
+// 組件 ----
+
+</style>
