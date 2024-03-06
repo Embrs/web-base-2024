28d4a9c88dc8a3eeec8d8645953264d62c5de168
鄒豐懋 <zoufengmao@zoufengmaodeMacBook-Pro.local>
2024-03-06 17:10:34 +0800

    backup

---
+++ components/global/el-image-plus.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+// AtImg 等比縮放，懶加載 圖片
+import { Picture as IconPicture } from '@element-plus/icons-vue';
+
+// 資料 --------------------------------------------------------------------------------------------
+const props = defineProps({
+  width: {
+    type: Number,
+    default: 100 // 寬，100~0 為 %
+  },
+  height: {
+    type: [Number, String],
+    default: 100 // 高，100~0 為 %, useHeightScale = true 時為寬的比例
+  },
+  useScale: { // 使用比例縮放
+    type: Boolean,
+    default: false
+  }
+});
+
+// 圖片限制樣式
+const imgBoxStyle = computed(() => {
+  const _style: any = {};
+  _style.width = `${props.width}${props.width <= 100 ? '%' : 'px'} !important`;
+
+  if (props.useScale) {
+    _style['padding-top'] = `${props.height}% !important`;
+  } else {
+    _style.height = `${props.height}${Number(props.height) <= 100 ? '%' : 'px'} !important`;
+  }
+  return _style;
+});
+
+</script>
+
+<template lang="pug">
+#AtImg(:style="imgBoxStyle")
+  ElImage.img(
+    :lazy="true"
+    alt="我是圖片"
+    fit="cover"
+    v-bind="$attrs"
+  )
+    template(#error)
+      .err-slot
+        ElIcon
+          IconPicture
+</template>
+
+<style lang="scss" scoped>
+// 佈局 ----
+#AtImg {
+  position: relative;
+  overflow: hidden;
+}
+// 組件 ----
+.img {
+  @include wh;
+  @include absolute;
+  display: block;
+  -webkit-user-drag: none; // 不可拖曳圖片
+  -webkit-touch-callout: none; // ios禁用菜單
+  pointer-events: none; // 防止拖拉圖片
+}
+.err-slot {
+  @include wh;
+  @include center;
+  font-size: 30px;
+  color: var(--el-text-color-secondary);
+  background: var(--el-fill-color-light);
+}
+</style>
