b93ffcc5edd44baf3ffd24f0b715748b1ed1946b
AthenaHarry <harry@athenaads.com>
2024-05-28 10:46:40 +0800

    fix build error

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import OpenGroup from '@/components/open-group/index.vue';
+import OpenGroup from '@/components/open-group/index.vue'; // 不能放進app.vue會壞掉
 // layout
 import LoadingPage from '@/components/layout/loading-page.vue';
 </script>
@@ -7,9 +7,6 @@ import LoadingPage from '@/components/layout/loading-page.vue';
 <template lang="pug">
 #layout
   LoadingPage
-  //- p layout
-  //- pre(v-for="i of 100" :key="i") {{ "123231212123231212312aa123231212312aa123231212312aa123231212312aa123231212312aa123231212312aa312aa" }}
-  //- // TODO check
   slot
   ClientOnly
     OpenGroup
