13bf2b0bced944ab630dc8e335e1de1097253f34
AthenaHarry <harry@athenaads.com>
2024-06-14 16:21:40 +0800

    token maxAge

--- composables/stores/store-user.ts
+++ composables/stores/store-user.ts
@@ -1,8 +1,8 @@
 // export const storeDemo = () => useState('storeDemo', () => {}); // 不可用，有 bug
-
+const sevenDay = 604800;
 export const StoreUser = pinia.defineStore('StoreUser', () => {
   // token -----------------------------------------------------------------------------------------------
-  const token = useCookie<string>('token', { default: () => '' }); // 登入狀態
+  const token = useCookie<string>('token', { default: () => '', maxAge: sevenDay }); // 登入狀態
   /** 是否登入 */
   const isSignIn = computed(() => !!token.value);
   /** 設定 Token */
