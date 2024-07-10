47df47600d88a8f3f846a020e454051efb431e5a
AthenaHarry <harry@athenaads.com>
2024-07-10 09:57:42 +0800

    fix env

--- composables/mixins/mixin-init-meta.ts
+++ composables/mixins/mixin-init-meta.ts
@@ -2,6 +2,7 @@
 export const MixinInitMeta = () => {
   // 資料 --------------------------------------------------------------------------------------------
   const route = useRoute();
+  const author = useState('author', () => 'Athenaads');
   const seoTitle = useState('seoTitle', () => '網站名稱');
   const seoDesc = useState('seoDesc', () => '網站描述');
   const seoKeywords = useState('seoKeywords', () => 'aa,bb,cc');
@@ -21,9 +22,9 @@ export const MixinInitMeta = () => {
       // { name: 'robots', content: 'index' }, // content: https://www.newscan.com.tw/all-seo/robots-meta-directives.htm
       { name: 'description', content: seoDesc.value },
       { name: 'keywords', content: seoKeywords.value },
-      { name: 'copyright', content: 'Copyright © 2024 Athenaads. All rights reserved.' },
+      { name: 'copyright', content: `Copyright © 2024 ${author.value}. All rights reserved.` },
       { name: 'revisit-after', content: '1 days' },
-      { name: 'author', content: 'Athenaads' },
+      { name: 'author', content: author.value },
 
       { property: 'og:title', content: seoTitle.value },
       // { property: 'og:type', content: '類型' },
@@ -37,7 +38,7 @@ export const MixinInitMeta = () => {
       { property: 'twitter:title', content: seoTitle.value },
       { property: 'twitter:description', content: seoDesc.value },
       // { property: 'twitter:site', content: '帳號' },
-      { property: 'twitter:creator', content: 'Athenaads' },
+      { property: 'twitter:creator', content: author.value },
       { property: 'twitter:url', content: `${route.path}` },
       { property: 'twitter:image', content: '/img/share.jpg' }
     ]
